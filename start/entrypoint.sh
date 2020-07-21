f6c4815372873b8aea719187f22633ff3ae07838
kaissi <mrkaissi@gmail.com>
2020-07-21 01:24:59 -0300

    PER-327 Publicando imagem Laravel - Utilizando Dockerize para Wait e Template

--- start/entrypoint.sh
+++ start/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo ">> Waiting for MySQL be ready..."
-
-until curl -f -s http://db:${MYSQL_HEALTH_PORT_TARGET}/readiness > /dev/null; do
-    sleep 1
-done;
-
-echo ">> MySQL ready!"
-
 echo ">> Executing 'composer install'"
 composer install
 
